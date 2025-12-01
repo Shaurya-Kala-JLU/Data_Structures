3b1ed97156edfadad7447a46daa1230b20c74c2a
Shaurya-Kala-JLU <jlu09313@student.jlu.edu.in>
2025-12-01 11:31:10 +0530

    Add files via upload

---
+++ Ex30.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+void quicksort(int a[], int low, int high) {
+    int i = low, j = high;
+    int pivot = a[low]; 
+    int temp;
+    if (low < high) {
+        while (i < j) {
+            while (a[i] <= pivot && i <= high) 
+                i++;
+            while (a[j] > pivot && j >= low) 
+                j--;
+            if (i < j) {
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+        a[low] = a[j];
+        a[j] = pivot;
+        quicksort(a, low, j - 1);
+        quicksort(a, j + 1, high);
+    }
+}
+int main() {
+    int n, i;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int a[n];
+    printf("Enter elements:\n");
+    for (i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+    quicksort(a, 0, n - 1);
+    printf("Sorted array:\n");
+    for (i = 0; i < n; i++)
+        printf("%d ", a[i]);
+    return 0;
+}
